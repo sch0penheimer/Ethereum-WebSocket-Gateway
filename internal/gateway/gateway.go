10a65516a909736cb202ecf22bc5f2cc2aa606a6
Lenovo <machine.bidi66@gmail.com>
2025-07-15 20:56:57 +0100

    - chore: Created internal/gateway package with initial gateway.go placeholder file.

---
+++ internal/gateway/gateway.go
@@ -0,0 +1,4 @@
+package gateway
+
+// Core types and interfaces for the gateway logic will be moved here from blockchain and websocket packages.
+// This is the initial file setup for the refactoring process. 
