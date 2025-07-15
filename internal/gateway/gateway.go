4e50d7507285b246a9723e59fe556fb22282ab0f
Lenovo <machine.bidi66@gmail.com>
2025-07-15 21:04:51 +0100

    - chore: Added pro header doc of the newlly added package file.

--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -1,4 +1,123 @@
-package gateway
+/*
+==========================================================================================
+  File:        gateway.go
+  Last Update: 2024-05-18
+  Author:      Haitam Bidiouane (@sh0penheimer)
+  Ownership:   © Haitam Bidiouane. All rights reserved.
+------------------------------------------------------------------------------------------
+  Scope:
+    Defines core gateway types and interfaces for blockchain and websocket management.
+    Provides abstractions for block fetching, mining control, and websocket client handling.
+==========================================================================================
+*/
+//----------------------------------------------------------------------------------------------------------------//
+//-- Core types and interfaces for the gateway logic will be moved here from blockchain and websocket packages. --//
+//-- This is the initial file setup for the refactoring process. --//
+//----------------------------------------------------------------------------------------------------------------//
 
-// Core types and interfaces for the gateway logic will be moved here from blockchain and websocket packages.
-// This is the initial file setup for the refactoring process. 
+import (
+    "context"
+    "sync"
+    "time"
+    "math/big"
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/rpc"
+    "github.com/gorilla/websocket"
+    "encoding/json"
+)
+
+type BlockTransaction struct {
+    Hash  string  `json:"hash"`
+    From  string  `json:"from"`
+    To    string  `json:"to"`
+    Value float64 `json:"value"`
+}
+
+type Block struct {
+    Number           uint64             `json:"number"`
+    Hash             string             `json:"hash"`
+    ParentHash       string             `json:"parentHash"`
+    Sha3Uncles       string             `json:"sha3uncles"`
+    TransactionRoot  string             `json:"transactionRoot"`
+    Timestamp        time.Time          `json:"timestamp"`
+    Validator        string             `json:"validator"`
+    Size             uint64             `json:"size"`
+    GasUsed          uint64             `json:"gasUsed"`
+    GasLimit         uint64             `json:"gasLimit"`
+    Transactions     []BlockTransaction `json:"transactions"`
+    TransactionCount int                `json:"transactionCount"`
+    TotalFees        float64            `json:"totalFees"`
+}
+
+type BlockFetcher struct {
+    Client    *ethclient.Client
+    RPCClient *rpc.Client
+}
+
+type MiningController struct {
+    clients []*rpc.Client
+    mu      sync.Mutex
+}
+
+type WSHandler struct {
+    blockFetcher     *BlockFetcher
+    miningController *MiningController
+    clients          map[*Client]bool
+    subscriptions    map[*Client]bool
+    register         chan *Client
+    unregister       chan *Client
+    broadcast        chan []byte
+    mu               sync.Mutex
+}
+
+type Client struct {
+    conn *websocket.Conn
+    send chan []byte
+}
+
+type WSMessage struct {
+    Type    string          `json:"type"`
+    Payload json.RawMessage `json:"payload"`
+}
+
+type LatestBlocksRequest struct {
+    Count int `json:"count"`
+}
+
+type MiningRequest struct {
+    Start bool `json:"start"`
+}
+
+// Function signatures (implementations will be moved/refactored next)
+func NewBlockFetcher(nodeURL string) (*BlockFetcher, error) { return nil, nil }
+func (bf *BlockFetcher) GetLatestBlocks(ctx context.Context, count int) ([]Block, error) { return nil, nil }
+func (bf *BlockFetcher) GetBlockByNumber(ctx context.Context, number *big.Int) (*Block, error) { return nil, nil }
+func (bf *BlockFetcher) GetValidators(ctx context.Context) ([]string, error) { return nil, nil }
+func (bf *BlockFetcher) GetNetworkMetrics(ctx context.Context) (map[string]interface{}, error) { return nil, nil }
+func (bf *BlockFetcher) calculateAverageBlockTime(ctx context.Context) (float64, error) { return 0, nil }
+func (bf *BlockFetcher) calculateNetworkLatency() float64 { return 0 }
+
+func NewMiningController(nodeURLs []string) (*MiningController, error) { return nil, nil }
+func (mc *MiningController) ToggleMining(ctx context.Context, start bool) ([]bool, error) { return nil, nil }
+func (mc *MiningController) GetMiningStatus(ctx context.Context) ([]bool, error) { return nil, nil }
+
+func NewWSHandler(blockFetcher *BlockFetcher, miningController *MiningController) *WSHandler { return nil }
+func (h *WSHandler) HandleConnections(w interface{}, r interface{}) {}
+func (h *WSHandler) writePump(client *Client) {}
+func (h *WSHandler) readPump(client *Client) {}
+func (h *WSHandler) handleSubscription(client *Client) {}
+func (h *WSHandler) handleLatestBlocks(conn *websocket.Conn, msg WSMessage) {}
+func (h *WSHandler) handleMiningStatus(conn *websocket.Conn) {}
+func (h *WSHandler) handleToggleMining(conn *websocket.Conn, msg WSMessage) {}
+func (h *WSHandler) watchNewBlocks() {}
+func (h *WSHandler) run() {}
+
+
+
+
+
+
+
+
